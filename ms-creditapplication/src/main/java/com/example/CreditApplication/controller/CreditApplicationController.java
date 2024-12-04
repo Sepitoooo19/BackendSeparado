208427faac5bc81ee7945ecde5dccbf6659a18c5
Sepitoooo19 <benjamin.sepulveda@usach.cl>
2024-12-04 20:30:17 -0300

    Cambios en gateway y ms

--- ms-creditapplication/src/main/java/com/example/CreditApplication/controller/CreditApplicationController.java
+++ ms-creditapplication/src/main/java/com/example/CreditApplication/controller/CreditApplicationController.java
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/credit-application")
+@RequestMapping("/credit-application/application")
 @CrossOrigin("*")
 public class CreditApplicationController {
 
