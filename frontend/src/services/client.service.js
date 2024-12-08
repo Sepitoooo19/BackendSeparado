deeaeb52944c8f654ba007f18bc804bb210cedfe
Sepitoooo19 <benjamin.sepulveda@usach.cl>
2024-12-08 17:29:01 -0300

    Cambio en client.service

--- frontend/src/services/client.service.js
+++ frontend/src/services/client.service.js
@@ -1,11 +1,12 @@
 import httpClient from '../http-common';
 
+
 const getAll = () => {
     return httpClient.get("user");
 }
 
 const create = data => {
-    return httpClient.post("new", data);
+    return httpClient.post("user/new", data);
 }
 
 const get = id => {
